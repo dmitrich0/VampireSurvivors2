c784f18d10d5aa9a4bcee92a0f6cbfbc43b5e2f6
Nikita <ivanovvnikita@mail.ru>
2022-06-04 10:39:22 +0500

    Добавил нового монстра: осу

--- VampireSurvivors2/View/Resources.Designer.cs
+++ VampireSurvivors2/View/Resources.Designer.cs
@@ -223,6 +223,26 @@ namespace VampireSurvivors2.View {
         /// <summary>
         ///   Поиск локализованного ресурса типа System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap bee {
+            get {
+                object obj = ResourceManager.GetObject("bee", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Поиск локализованного ресурса типа System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap bee2 {
+            get {
+                object obj = ResourceManager.GetObject("bee2", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Поиск локализованного ресурса типа System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap bg {
             get {
                 object obj = ResourceManager.GetObject("bg", resourceCulture);
